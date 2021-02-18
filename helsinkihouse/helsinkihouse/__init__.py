893bcb0a289566ce413627de0638982800da1a64
Alberto Nieto <nieto@student.chalmers.se>
2021-02-18 14:16:30 +0200

    Update python library

---
+++ helsinkihouse/helsinkihouse/__init__.py
@@ -0,0 +1,1 @@
+from .custom_funcs import parse_etuovi
--- helsinkihouse/helsinkihouse/config.py
+++ helsinkihouse/helsinkihouse/config.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
 data_dir = Path('C:/Users/Alberto/Dropbox/helsinki-house-price/data')
-data_path = data_dir / 'helsinki_house_price.xls'
-cleaned_data_path = data_dir / 'helsinki_house_price_cleaned.xls'
+data_path = data_dir / 'raw/helsinki_house_price.xls'
+cleaned_data_path = data_dir / 'cleaned/helsinki_house_price_cleaned.xls'
