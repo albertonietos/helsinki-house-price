b6fbc3a2a4ab0b9f8032a74223c34bea41ed7d7b
Alberto Nieto <45892957+albertonietos@users.noreply.github.com>
2025-06-14 21:56:36 +0200

    Fix deprecated st.cache (#7)
    Co-authored-by: Alberto Nieto Sandino <alberto.nieto.sandino@wolt.com>

--- app/helsinki-house-price-app.py
+++ app/helsinki-house-price-app.py
@@ -16,7 +16,7 @@ st.header("The Data")
 # Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
 # Load data into the dataframe.
-@st.cache
+@st.cache_data
 def load_data():
 	return pd.read_excel(DATA_URL, engine='xlrd')
 data = load_data()
